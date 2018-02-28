88ca90636f22c07e9172b87d987f37b8bdf0cbdf
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2018-03-01 00:06:34 +0200

    Popup if problem content is not accessible.

--- public/javascripts/homework1/controllers/contestProblemsPageController.js
+++ public/javascripts/homework1/controllers/contestProblemsPageController.js
@@ -4,12 +4,14 @@ angular.module('apisCallflow')
             $scope.problems = [];
             $scope.fetchingProblems = true;
             $scope.showingProblem = false;
+            $scope.problemNotAvailable = false;
 
             dataCollectorService
                 .getContestProblems($routeParams.contestId)
                 .then(fetchedContestProblemsCallback);
 
             $scope.getProblemContent = function (problem) {
+                $scope.problemNotAvailable = false;
                 dataCollectorService
                     .getProblemContent($routeParams.contestId, problem.index)
                     .then(getProblemContentCallback);
@@ -23,9 +25,14 @@ angular.module('apisCallflow')
             function getProblemContentCallback(response) {
                 console.log(response.data);
                 $scope.showedProblem = response.data;
-                $scope.showedProblem.sampleTest = zip($scope.showedProblem.sampleTest.input, $scope.showedProblem.sampleTest.output);
-                $scope.showingProblem = true;
-                $scope.$apply();
+                try {
+                    $scope.showedProblem.sampleTest = zip($scope.showedProblem.sampleTest.input, $scope.showedProblem.sampleTest.output);
+                    $scope.showingProblem = true;
+                } catch (error) {
+                    $scope.problemNotAvailable = true;
+                } finally {
+                    $scope.$apply();
+                }
             }
 
             function fetchedContestProblemsCallback(response) {
