a6e75b4538b641930da133f4ac08ac97c6fa185b
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2018-02-28 23:27:25 +0200

    Nice problem content arrangement.

--- public/javascripts/homework1/controllers/contestProblemsPageController.js
+++ public/javascripts/homework1/controllers/contestProblemsPageController.js
@@ -15,9 +15,15 @@ angular.module('apisCallflow')
                     .then(getProblemContentCallback);
             };
 
+            $scope.exitProblem = function () {
+                $scope.showingProblem = false;
+                $scope.$apply();
+            };
+
             function getProblemContentCallback(response) {
                 console.log(response.data);
                 $scope.showedProblem = response.data;
+                $scope.showedProblem.sampleTest = zip($scope.showedProblem.sampleTest.input, $scope.showedProblem.sampleTest.output);
                 $scope.showingProblem = true;
                 $scope.$apply();
             }
@@ -27,5 +33,16 @@ angular.module('apisCallflow')
                 $scope.fetchingProblems = false;
                 console.log($scope.problems);
                 $scope.$apply();
+            }
+
+            function zip(array1, array2) {
+                if (array1.length !== array2.length) {
+                    throw new Error('Arrays must have same size');
+                }
+                let zipArray = [];
+                for (let index = 0; index < array1.length; ++index) {
+                    zipArray.push([array1[index], array2[index]]);
+                }
+                return zipArray;
             }
         }]);
