d19f844d0fda502785989ed513a0b2351d2820db
DragosDorneanu <dragos.a.dorneanu@gmail.com>
2018-03-10 12:23:26 +0200

    Defensive coding and delete and replace todos handlers.

--- homework2/app.js
+++ homework2/app.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const uuid = require('uuid/v4');
+
+const httpUtils = require('./httpUtils');
 const TodosRequestHandler = require('./TodosRequestHandler');
 
 let todos = require('./todos');
@@ -15,29 +17,74 @@ function getTodos(request, response, todos) {
 }
 
 function createTodo(request, response, todos) {
-    if (request.headers['content-type'] !== 'application/json') {
-        response.statusCode = 400;
-        response.end();
-        return;
+    function successfulGotBody(newTodo) {
+        if (!newTodo['text'] || typeof(newTodo['completed']) === 'undefined') {
+            httpUtils.sendCustomResponse(response, 400, 'A todo should have "text" and "completed" properties.');
+            return;
+        }
+        const todoId = uuid();
+        todos[todoId] = newTodo;
+        httpUtils.sendCustomResponse(response, 201, undefined, todoId);
     }
 
-    let requestContent = '';
-    request.on('data', (data) => {
-        requestContent += data;
-    });
+    function failedToGetBody(error) {
+        console.error(error);
+        httpUtils.sendCustomResponse(response, 500);
+    }
 
-    request.on('end', () => {
-        const todo = JSON.parse(requestContent);
-        const todoId = uuid();
-        todos[todoId] = todo;
-        response.statusCode = 201;
-        response.end(todoId);
-    });
+    if (request.headers['content-type'] !== 'application/json') {
+        httpUtils.sendCustomResponse(response, 400);
+        return;
+    }
+    httpUtils
+        .getHttpBody(request)
+        .then(successfulGotBody)
+        .catch(failedToGetBody);
+}
 
-    request.on('error', () => {
-        response.statusCode = 500;
+function deleteTodos(request, response) {
+    todos = {};
+    response.end();
+}
+
+function replaceTodos(request, response, todos) {
+    function successfulReceivedNewTodos(newTodos) {
+        if (!(newTodos instanceof Array)) {
+            httpUtils.sendCustomResponse(response, 400, 'You must send a JSON Array.');
+            return;
+        }
+
+        let todo, todoId;
+        for (let todoId in todos) {
+            if (todos.hasOwnProperty(todoId)) {
+                delete todos[todoId];
+            }
+        }
+        for (let index = 0; index < newTodos.length; ++index) {
+            todo = newTodos[index];
+            if ((todo instanceof Object)) {
+                if (todo['text'] && todo['completed']) {
+                    todoId = uuid();
+                    todos[todoId] = todo;
+                }
+            }
+        }
         response.end();
-    });
+    }
+
+    function failedReceivingNewTodos(error) {
+        console.error(error);
+        httpUtils.sendCustomResponse(response, 500);
+    }
+
+    if (request.headers['content-type'] !== 'application/json') {
+        httpUtils.sendCustomResponse(response, 400);
+        return;
+    }
+    httpUtils
+        .getHttpBody(request)
+        .then(successfulReceivedNewTodos)
+        .catch(failedReceivingNewTodos);
 }
 
 todosRequestHandler.addRequestHandler('/todos', 'GET', (request, response) => {
@@ -47,6 +94,12 @@ todosRequestHandler.addRequestHandler('/todos', 'GET', (request, response) => {
 todosRequestHandler.addRequestHandler('/todos', 'POST', (request, response) => {
     createTodo(request, response, todos);
 });
+
+todosRequestHandler.addRequestHandler('/todos', 'PUT', (request, response) => {
+    replaceTodos(request, response, todos);
+});
+
+todosRequestHandler.addRequestHandler('/todos', 'DELETE', deleteTodos);
 
 
 const server = http.createServer(todosRequestHandler.handleRequest);
---
+++ homework2/httpUtils.js
@@ -0,0 +1,27 @@
+function getHttpBody(httpEntity) {
+    return new Promise((resolve, reject) => {
+        let content = '';
+        httpEntity.on('data', (data) => {
+            content += data;
+        });
+
+        httpEntity.on('end', () => {
+            content = JSON.parse(content);
+            resolve(content);
+        });
+
+        httpEntity.on('error', (error) => {
+            reject(error);
+        });
+    });
+}
+
+function sendCustomResponse(response, statusCode, statusMessage, responseBody) {
+    response.statusCode = statusCode;
+    if (statusMessage) {
+        response.statusMessage = statusMessage;
+    }
+    response.end(responseBody);
+}
+
+module.exports = {getHttpBody, sendCustomResponse};
